356fa808fa1acd4a278b220b908a21e7adfc56b1
Deepika0196 <39754908+Deepika0196@users.noreply.github.com>
2023-08-04 10:45:03 +0530

    Update code.py

--- challenge2/code.py
+++ challenge2/code.py
@@ -1,26 +1,23 @@
 import requests
 
-def get_azure_instance_metadata(metadata_endpoint="http://169.254.169.254/metadata/instance?api-version=2021-02-01"):
+def get_metadata(metadata_endpoint="http://169.254.169.254/metadata"):
     headers = {"Metadata": "true"}
     try:
         response = requests.get(metadata_endpoint, headers=headers, timeout=2)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
-        print("Error while fetching metadata:", e)
+        print("Error :", e)
         return None
-
-# Call the function to get the instance metadata
-instance_metadata = get_azure_instance_metadata()
-
-# Check if the metadata is available and print some of the properties
-if instance_metadata:
+ 
+metadata = get_metadata()
+if metadata:
     print("Azure Instance Metadata:")
-    print("VM ID:", instance_metadata["compute"]["vmId"])
-    print("VM Size:", instance_metadata["compute"]["vmSize"])
-    print("Location:", instance_metadata["compute"]["location"])
-    print("Resource Group:", instance_metadata["compute"]["resourceGroupName"])
-    print("Network Interface ID:", instance_metadata["network"]["interface"][0]["id"])
-    # You can print more properties based on your requirement
+    print("VM ID:", metadata["compute"]["vmId"])
+    print("VM Size:", metadata["compute"]["vmSize"])
+    print("Location:", metadata["compute"]["location"])
+    print("Resource Group:", metadata["compute"]["resourceGroupName"])
+    print("Network Interface ID:", metadata["network"]["interface"][0]["id"])
+     
 else:
-    print("Metadata not available. Are you running this code within an Azure VM?")
+    print("Metadata not available")
